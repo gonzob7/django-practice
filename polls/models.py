16269445362c16518bbb393fd042184e624737b9
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-25 17:25:30 -0800

    created question model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.Charfield(max_length=200)
+    pub_date = models.DateTimeField('date published')
