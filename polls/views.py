638851608e5b1b44035b1b10fd080de92ec1c90c
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-25 18:48:27 -0800

    added more views

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+'''views with question number you're currently on'''
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request, question_id):
+    response = "You're viewing results for question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
