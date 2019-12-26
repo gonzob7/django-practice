d644992438dffc830aa5f9e69a9e410c75d6a651
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-25 19:18:40 -0800

    updated main page to display questions with hyperlinks

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question
 # Create your views here.
 
 
 def index(request):
     latest_questions = Question.objects.order_by("-pub_date")[:5]
-    output = ', '.join([q.question_text for q in latest_questions])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_questions':latest_questions,
+    }
+    return HttpResponse(template.render(context, request))
 
 '''views with question number you're currently on'''
 def detail(request, question_id):
