22f28eaf12046fab5820ee13b11bbaaf9e7afc27
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-25 17:32:40 -0800

    installed polls app in settings

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.Charfield(max_length=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
