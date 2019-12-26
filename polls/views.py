a5270996052459ea7583ba61fa5e13e3911ce2e7
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-25 18:54:45 -0800

    imported Question class from models into views file

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 
 def index(request):
+
     return HttpResponse("Hello, world. You're at the polls index.")
 
 '''views with question number you're currently on'''
